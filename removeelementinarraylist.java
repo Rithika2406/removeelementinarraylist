66dbd30a94164a396ee2c173b9cd7b39e4cf0c0c
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2025-04-04 17:51:00 +0530

    Add files via upload

---
+++ removeelementinarraylist.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Main
+{
+	public static void main(String[] args) {
+	    ArrayList<Integer>number=new ArrayList<Integer>();
+	    number.add(1);
+	    number.add(2);
+	    number.add(3);
+	    number.remove(0);
+		System.out.println(number);
+	}
+}
